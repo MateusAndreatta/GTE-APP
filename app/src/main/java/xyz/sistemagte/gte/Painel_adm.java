8061dd77486674a0692e872f5448630564d54522
Andreatta <30483277+MateusAndreatta@users.noreply.github.com>
2018-03-09 16:11:30 -0300

    java do painel (back btn)

--- app/src/main/java/xyz/sistemagte/gte/Painel_adm.java
+++ app/src/main/java/xyz/sistemagte/gte/Painel_adm.java
@@ -1,7 +1,9 @@
 package xyz.sistemagte.gte;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Painel_adm extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class Painel_adm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painel_adm);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
+        getSupportActionBar().setTitle("Painel");     //Titulo para ser exibido na sua Action Bar em frente à seta
+
+
+    }
+    //este é para o da navbar (seta)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, Login.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    //O botao padrao do android
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this, Login.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 }
