4d5f3a23f17b686ec2e3bc020169b9526c254f0b
Andreatta <30483277+MateusAndreatta@users.noreply.github.com>
2018-04-03 17:04:17 -0300

    back btn em painel monitora, motorista

--- app/src/main/java/xyz/sistemagte/gte/Painel_motorista.java
+++ app/src/main/java/xyz/sistemagte/gte/Painel_motorista.java
@@ -1,10 +1,13 @@
 package xyz.sistemagte.gte;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Painel_motorista extends AppCompatActivity {
-
+    TextView Label;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +15,33 @@ public class Painel_motorista extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
         getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
         getSupportActionBar().setTitle(getResources().getString(R.string.AppBarPainel));
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String UserNome = bundle.getString("BundleUserNome");
+
+        Label = findViewById(R.id.LabelOlaPainel);
+        Label.setText(Label.getText() + " " + UserNome);
+    }
+
+    //este é para o da navbar (seta)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, Login.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    //O botao padrao do android
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this, Login.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 }
