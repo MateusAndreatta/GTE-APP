00dcfc1e5854692e6a0f4ba4c9c22fac2bc711bf
Andreatta <30483277+MateusAndreatta@users.noreply.github.com>
2018-04-10 16:34:02 -0300

    editar perfil resp

--- app/src/main/java/xyz/sistemagte/gte/Editar_perfil_resp.java
+++ app/src/main/java/xyz/sistemagte/gte/Editar_perfil_resp.java
@@ -1,25 +1,56 @@
 package xyz.sistemagte.gte;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import xyz.sistemagte.gte.Auxiliares.GlobalUser;
 
 public class Editar_perfil_resp extends AppCompatActivity {
 
-    private int idEmpresa;
+    private int idEmpresa,idUsuario;
+
+    EditText Nome,Sobrenome, Email,DataNasc,Cpf,Rg ;
+
+    RequestQueue requestQueue;
+
+    String NomeHolder,SobrenomeHolder, EmailHolder,DataNascHolder,CpfHolder,RgHolder;
+
+    ProgressDialog progressDialog;
+
+    String HttpUrl = "https://sistemagte.xyz/android/editar/editarResponsavel.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil_resp);
 
+        Nome      = findViewById(R.id.editar_nome);
+        Sobrenome = findViewById(R.id.editar_sobrenome);
+        Email     = findViewById(R.id.editar_email);
+        DataNasc  = findViewById(R.id.editar_datanascimento);
+        Cpf   = findViewById(R.id.editar_cpf);
+        Rg   = findViewById(R.id.editar_rg);
 
         GlobalUser global =(GlobalUser)getApplication();
         idEmpresa = global.getGlobalUserIdEmpresa();
+        idUsuario = global.getGlobalUserID();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
         getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
         getSupportActionBar().setTitle(getResources().getString(R.string.editar_perfil));     //Titulo para ser exibido na sua Action Bar em frente à seta
@@ -45,4 +76,75 @@ public class Editar_perfil_resp extends AppCompatActivity {
         finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
         return;
     }
+
+
+
+    public void salvarEdicao(View view) {
+
+        // Showing progress dialog at user registration time.
+        progressDialog.setMessage(getResources().getString(R.string.loadingMsg));
+        progressDialog.show();
+
+        // Calling method to get value from EditText.
+        GetValueFromEditText();
+
+        // Creating string request with post method.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String ServerResponse) {
+
+                        // Hiding the progress dialog after all task complete.
+                        progressDialog.dismiss();
+
+                        // Showing response message coming from server.
+                        Toast.makeText(Editar_perfil_resp.this, getResources().getString(R.string.MsgSucesso), Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                        // Hiding the progress dialog after all task complete.
+                        progressDialog.dismiss();
+
+                        // Showing error message if something goes wrong.
+                        Toast.makeText(Editar_perfil_resp.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                // Creating Map String Params.
+                Map<String, String> params = new HashMap<String, String>();
+
+                // Adding All values to Params.
+                params.put("id", String.valueOf(idUsuario));
+                params.put("nome", NomeHolder);
+                params.put("sobrenome", SobrenomeHolder);
+                params.put("email", EmailHolder);
+                params.put("data", DataNascHolder);
+                params.put("cpf", CpfHolder);
+                params.put("rg", RgHolder);
+
+                return params;
+            }
+
+        };
+
+        requestQueue.getCache().clear();
+        requestQueue.add(stringRequest);
+        Toast.makeText(this, getResources().getString(R.string.informacoesSalvasSucesso), Toast.LENGTH_SHORT).show();
+    }
+
+    // Creating method to get value from EditText.
+    public void GetValueFromEditText(){
+        NomeHolder = Nome.getText().toString().trim();
+        SobrenomeHolder = Sobrenome.getText().toString().trim();
+        EmailHolder = Email.getText().toString().trim();
+        DataNascHolder = DataNasc.getText().toString().trim();
+        CpfHolder = Cpf.getText().toString().trim();
+        RgHolder = Rg.getText().toString().trim();
+    }
+
 }
