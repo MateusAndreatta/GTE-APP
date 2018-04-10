3b0fdb8529a63f1f4ca22b80958e6cbc3c2cc1ea
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-04-10 16:26:13 -0300

    editar monitora

---
+++ app/src/main/java/xyz/sistemagte/gte/editarMonitora.java
@@ -0,0 +1,13 @@
+package xyz.sistemagte.gte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editarMonitora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_monitora);
+    }
+}
