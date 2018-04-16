5c0dde5fce02558dac6215df63e6a645535c2a38
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-04-16 18:24:47 -0300

    continuação do cad de criança
    Co-Authored-By: Andreatta <mateusandreatta@users.noreply.github.com>

--- app/src/main/java/xyz/sistemagte/gte/cad_crianca.java
+++ app/src/main/java/xyz/sistemagte/gte/cad_crianca.java
@@ -17,11 +17,13 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import xyz.sistemagte.gte.Auxiliares.GlobalUser;
 import xyz.sistemagte.gte.Construtoras.CriancaConst;
 
 public class cad_crianca extends AppCompatActivity {
@@ -36,17 +38,26 @@ public class cad_crianca extends AppCompatActivity {
 
     String NomeHolder,SobrenomeHolder,DataNascHolder,CpfHolder,RgHolder, CidadeHolder,CEPHolder,TelefoneHolder,NumeroHolder,RuaHolder, ComplementoHolder, EstadoHolder;
 
-    String HttpUrl = "https://sistemagte.xyz/android/cadastros/cadCrianca.php";
+    String HttpUrl = "https://sistemagte.xyz/android/cadastros/cadCriancaResp.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_crianca);
 
+        requestQueue = Volley.newRequestQueue(this);
+
+        progressDialog = new ProgressDialog(cad_crianca.this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
         getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
         getSupportActionBar().setTitle(getResources().getString(R.string.cadastro_crianca));
 
+        GlobalUser global =(GlobalUser)getApplication();
+        idUsuario = global.getGlobalUserID();
+        idEmpresa = global.getGlobalUserIdEmpresa();
+
+        //TODO: preencher o spinner escolas com as escolas cadastradas no banco referente a uma devida empresa do usuario
     }
 
     //este é para o da navbar (seta)
@@ -88,65 +99,84 @@ public class cad_crianca extends AppCompatActivity {
 
     public void Cadastrar(View view) {
 
-        // Showing progress dialog at user registration time.
-        progressDialog.setMessage(getResources().getString(R.string.loadingMsg));
-        progressDialog.show();
+        if(VerificarCampos()) {
 
-        // Calling method to get value from EditText.
-        GetValueFromEditText();
 
-        // Creating string request with post method.
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String ServerResponse) {
+            // Showing progress dialog at user registration time.
+            progressDialog.setMessage(getResources().getString(R.string.loadingMsg));
+            progressDialog.show();
 
-                        // Hiding the progress dialog after all task complete.
-                        progressDialog.dismiss();
+            // Calling method to get value from EditText.
+            GetValueFromEditText();
 
-                        // Showing response message coming from server.
-                        Toast.makeText(cad_crianca.this, getResources().getString(R.string.MsgSucesso), Toast.LENGTH_LONG).show();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
+            // Creating string request with post method.
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String ServerResponse) {
 
-                        // Hiding the progress dialog after all task complete.
-                        progressDialog.dismiss();
+                            // Hiding the progress dialog after all task complete.
+                            progressDialog.dismiss();
 
-                        // Showing error message if something goes wrong.
-                        Toast.makeText(cad_crianca.this, volleyError.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }) {
-            @Override
-            protected Map<String, String> getParams() {
+                            // Showing response message coming from server.
+                            Toast.makeText(cad_crianca.this, getResources().getString(R.string.informacoesSalvasSucesso), Toast.LENGTH_SHORT).show();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
 
-                // Creating Map String Params.
-                Map<String, String> params = new HashMap<>();
+                            // Hiding the progress dialog after all task complete.
+                            progressDialog.dismiss();
 
-                // Adding All values to Params.
-                params.put("id", String.valueOf(idUsuario));
-                params.put("nome", NomeHolder);
-                params.put("sobrenome", SobrenomeHolder);
-                params.put("data", DataNascHolder);
-                params.put("cpf", CpfHolder);
-                params.put("rg", RgHolder);
-                params.put("tel", TelefoneHolder);
-                params.put("cep", CEPHolder);
-                params.put("cidade", CidadeHolder);
-                params.put("rua", RuaHolder);
-                params.put("numero", NumeroHolder);
-                params.put("complemento", ComplementoHolder);
-                params.put("estado", EstadoHolder);
+                            // Showing error message if something goes wrong.
+                            Toast.makeText(cad_crianca.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() {
 
-                return params;
-            }
+                    // Creating Map String Params.
+                    Map<String, String> params = new HashMap<>();
 
-        };
+                    // Adding All values to Params.
+                    params.put("id", String.valueOf(idUsuario));
+                    params.put("nome", NomeHolder);
+                    params.put("sobrenome", SobrenomeHolder);
+                    params.put("data", DataNascHolder);
+                    params.put("cpf", CpfHolder);
+                    params.put("rg", RgHolder);
+                    params.put("tel", TelefoneHolder);
+                    params.put("cep", CEPHolder);
+                    params.put("cidade", CidadeHolder);
+                    params.put("rua", RuaHolder);
+                    params.put("numero", NumeroHolder);
+                    params.put("complemento", ComplementoHolder);
+                    params.put("estado", EstadoHolder);
 
-        requestQueue.getCache().clear();
-        requestQueue.add(stringRequest);
-        Toast.makeText(this, getResources().getString(R.string.informacoesSalvasSucesso), Toast.LENGTH_SHORT).show();
+                    return params;
+                }
+
+            };
+
+            requestQueue.getCache().clear();
+            requestQueue.add(stringRequest);
+        }
+    }
+
+    private boolean VerificarCampos(){
+    //EditText Nome,Sobrenome,Telefone, CEP,DataNasc,Cpf,Rg,Cidade, Rua, Numero, Complemento;
+        //        Spinner Estado;
+        //TODO: Verificar o spiner tbm
+        //lembrando q tem 2 o das escolas e dos estados
+        if(Nome.getText().length() == 0 || Sobrenome.getText().length() == 0 || Telefone.getText().length() == 0 || CEP.getText().length() == 0
+                || DataNasc.getText().length() == 0 || Cpf.getText().length() == 0 || Rg.getText().length() == 0 || Cidade.getText().length() == 0
+                || Rua.getText().length() == 0 || Numero.getText().length() == 0){
+            Toast.makeText(this, getResources().getString(R.string.verificarCampos), Toast.LENGTH_SHORT).show();
+            return false;
+        }else
+        {
+            return true;
+        }
     }
 }
