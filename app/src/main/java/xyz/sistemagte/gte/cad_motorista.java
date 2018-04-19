f1883638589009af0826937c65a52bbf1eeb37e2
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-04-19 16:07:46 -0300

    outras mascaras
    Co-Authored-By: Andreatta <mateusandreatta@users.noreply.github.com>

--- app/src/main/java/xyz/sistemagte/gte/cad_motorista.java
+++ app/src/main/java/xyz/sistemagte/gte/cad_motorista.java
@@ -1,13 +1,47 @@
 package xyz.sistemagte.gte;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class cad_motorista extends AppCompatActivity {
+import br.com.jansenfelipe.androidmask.MaskEditTextChangedListener;
+
+
+public class cad_motorista extends AppCompatActivity{
+
+    EditText cep,cidade,rua,numero,complemento,cnh,validaCnh;
+    Spinner sexo,categoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_motorista);
+
+        cep = findViewById(R.id.cad_cep);
+        cidade = findViewById(R.id.cad_cidade);
+        rua = findViewById(R.id.cad_rua);
+        numero = findViewById(R.id.cad_num);
+        complemento = findViewById(R.id.cad_complemento);
+        cnh = findViewById(R.id.cad_cnh);
+        validaCnh = findViewById(R.id.cad_ValidCnh);
+
+        categoria = findViewById(R.id.cad_categoria);
+        sexo = findViewById(R.id.cad_sexo);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
+        getSupportActionBar().setTitle(getResources().getString(R.string.cadastro_motorista));
+
+        MaskEditTextChangedListener mascaraCPE = new MaskEditTextChangedListener("#####-###",cep);
+        MaskEditTextChangedListener mascaraCNH = new MaskEditTextChangedListener("###########",cnh);
+        MaskEditTextChangedListener mascaraValida  = new MaskEditTextChangedListener("##/##/####",validaCnh);
+
+        cep.addTextChangedListener(mascaraCPE);
+        cnh.addTextChangedListener(mascaraCNH);
+        validaCnh.addTextChangedListener(mascaraValida);
+
+
+
     }
 }
