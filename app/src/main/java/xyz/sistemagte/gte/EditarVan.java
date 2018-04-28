490f566e9a0d0af8f1e1a349191d02f404963e1e
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-04-28 11:23:23 -0300

    Edição de vans

---
+++ app/src/main/java/xyz/sistemagte/gte/EditarVan.java
@@ -0,0 +1,42 @@
+package xyz.sistemagte.gte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.jansenfelipe.androidmask.MaskEditTextChangedListener;
+
+public class EditarVan extends AppCompatActivity {
+
+    EditText capacidade, modelo, placa,ano,marca;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_van);
+
+        capacidade = findViewById(R.id.cad_capacidade);
+        modelo = findViewById(R.id.cad_modelo);
+        placa = findViewById(R.id.cad_placa);
+        ano = findViewById(R.id.cad_ano_fab);
+        marca = findViewById(R.id.cad_marca);
+
+        MaskEditTextChangedListener mascaraPlaca = new MaskEditTextChangedListener("###-###",placa);
+        MaskEditTextChangedListener mascaraAno = new MaskEditTextChangedListener("####",ano);
+
+        placa.addTextChangedListener(mascaraPlaca);
+        ano.addTextChangedListener(mascaraAno);
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(true);      //Ativar o botão
+        getSupportActionBar().setTitle(getResources().getString(R.string.EditarVan));
+
+    }
+    public void editarVan(View view) {
+        Toast.makeText(this, "Não disponivel no momento!", Toast.LENGTH_SHORT).show();
+    }
+}
