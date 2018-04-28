d63fd0c6c29d181b830ceaa3af76b0b4448a40d6
Andreatta <mateus-henrique2001@hotmail.com>
2018-04-28 01:51:06 -0300

    Login com facebook funcionando

--- app/src/main/java/xyz/sistemagte/gte/Login.java
+++ app/src/main/java/xyz/sistemagte/gte/Login.java
@@ -9,6 +9,7 @@ import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,11 +27,14 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
@@ -59,6 +63,7 @@ public class Login extends AppCompatActivity {
     ProgressDialog progressDialog;
 
     String HttpUrl = "https://sistemagte.xyz/android/login.php";
+    String HttpFacebookLogin = "https://sistemagte.xyz/android/loginFB.php";
     LoginButton loginButton;
     CallbackManager callbackManager;
     @Override
@@ -90,18 +95,44 @@ public class Login extends AppCompatActivity {
         txtBottomSheet = findViewById(R.id.txtBottomSheet1);
         btnLogin = findViewById(R.id.btnLogin);
 
-
         iniciarComponentes();
         loginFacebook();
-
     }
 
     private void loginFacebook(){
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(Login.this, "sucesso", Toast.LENGTH_SHORT).show();
-                Toast.makeText(Login.this, loginResult.toString(), Toast.LENGTH_SHORT).show();
+                System.out.println("onSuccess");
+                String accessToken = loginResult.getAccessToken().getToken();
+                Log.i("accessToken", accessToken);
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        if (response.getError() != null) {
+                            // handle error
+                            Toast.makeText(Login.this, "Erro no response", Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            String user_lastname = object.optString("last_name");
+                            String user_firstname = object.optString("first_name");
+                            String user_email = response.getJSONObject().optString("email");
+
+
+                            Log.i("Last name: ", user_lastname);
+                            Log.i("name: ",user_firstname);
+                            Log.i("email: ",user_email);
+                            enviarEmailFB(user_email);
+                        }
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "last_name,first_name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
 
             @Override
@@ -124,6 +155,7 @@ public class Login extends AppCompatActivity {
 
     private void iniciarComponentes(){
         loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
         callbackManager = CallbackManager.Factory.create();
     }
 
@@ -251,6 +283,94 @@ public class Login extends AppCompatActivity {
                 // Adding All values to Params.
                 params.put("emailAPP", EmailHolder);
                 params.put("senhaAPP", SenhaHolder);
+
+                return params;
+            }
+
+        };
+
+        requestQueue.getCache().clear();
+        requestQueue.add(stringRequest);
+    }
+
+    private void enviarEmailFB(final String emailFB){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpFacebookLogin,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String ServerResponse) {
+                        switch (ServerResponse.trim()){
+                            case "UsuarioNaoCadastrado":
+                                Toast.makeText(Login.this, getResources().getString(R.string.LoginEmailNaoCadastrado), Toast.LENGTH_SHORT).show();
+                                break;
+
+                            case "UsuarioSemPermicao":
+                                Toast.makeText(Login.this, getResources().getString(R.string.LoginPermissao), Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                //nenhum erro
+
+                                if(Objects.equals(ServerResponse, "UsuarioNaoCadastrado")){
+                                    Toast.makeText(Login.this, "Caiu no if", Toast.LENGTH_SHORT).show();
+                                }
+
+                                try{
+                                    JSONObject jsonObject = new JSONObject(ServerResponse);
+                                    JSONObject jsonArray = jsonObject.getJSONObject("nome");
+                                    String id = jsonArray.getString("id");
+                                    String tipo = jsonArray.getString("tipo");
+                                    String nome = jsonArray.getString("nome");
+                                    String sobrenome = jsonArray.getString("Sobrenome");
+                                    String empresa = jsonArray.getString("id empresa");
+                                    String email = jsonArray.getString("email");
+
+                                    GlobalUser global =(GlobalUser)getApplication();
+                                    global.setGlobalUserID(Integer.parseInt(id));
+                                    global.setGlobalUserTipoUser(Integer.parseInt(tipo));
+                                    global.setGlobalUserIdEmpresa(Integer.parseInt(empresa));
+                                    global.setGlobalUserNome(nome);
+                                    global.setGlobalUserSobrenome(sobrenome);
+                                    global.setGlobalUserEmail(email);
+
+                                    switch (tipo){
+                                        case("1"):
+                                            Intent telaMotorista = new Intent(Login.this, Painel_motorista.class);
+                                            startActivity(telaMotorista);
+                                            break;
+                                        case ("2"):
+                                            Intent telaResponsavel = new Intent(Login.this, Painel_responsavel.class);
+                                            startActivity(telaResponsavel);
+                                            break;
+                                        case ("3"):
+                                            Intent telaMonitora = new Intent(Login.this, Painel_monitora.class);
+                                            startActivity(telaMonitora);
+                                            break;
+                                        case ("4"):
+                                            Intent telaAdm = new Intent(Login.this, Painel_adm.class);
+                                            startActivity(telaAdm);
+                                            break;
+                                        default:
+                                            Toast.makeText(Login.this, getResources().getString(R.string.LoginPermissao), Toast.LENGTH_SHORT).show();
+                                            break;
+                                    }
+                                }catch (Exception ex){
+                                    Toast.makeText(Login.this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                                break;
+                        }//fechamento do switch
+                    }//onresponse
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Toast.makeText(Login.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                // Creating Map String Params.
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("emailAPP", emailFB);
 
                 return params;
             }
