34588e6e79e00fe4e722b6122377ed88b81f0e23
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-05-10 17:16:10 -0300

    Contrutora Motorista

--- app/src/main/java/xyz/sistemagte/gte/Construtoras/MotoristaConstr.java
+++ app/src/main/java/xyz/sistemagte/gte/Construtoras/MotoristaConstr.java
@@ -5,4 +5,28 @@ package xyz.sistemagte.gte.Construtoras;
  */
 
 public class MotoristaConstr {
+    String nome, sobrenome;
+    Integer id_motorista;
+
+    public MotoristaConstr(String nome, String sobrenome, Integer id_motorista) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.id_motorista = id_motorista;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public Integer getId_motorista() {
+        return id_motorista;
+    }
 }
+
+
+
+
