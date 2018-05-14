a1e36aeb3d956a7833362efa29a050a19b13d470
Luiz Felipe <31289072+CaMuFlaDin@users.noreply.github.com>
2018-05-14 17:21:23 -0300

    Tentiva  de search

--- app/src/main/java/xyz/sistemagte/gte/ListAdapters/ListViewCriancaResp.java
+++ app/src/main/java/xyz/sistemagte/gte/ListAdapters/ListViewCriancaResp.java
@@ -5,11 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import xyz.sistemagte.gte.Construtoras.CriancaConst;
+import xyz.sistemagte.gte.Construtoras.EscolasConstr;
 import xyz.sistemagte.gte.R;
 
 /**
@@ -19,6 +22,7 @@ import xyz.sistemagte.gte.R;
 public class ListViewCriancaResp extends ArrayAdapter<CriancaConst> {
 
     private List<CriancaConst> criancaList;
+    private List<CriancaConst> orig;
 
     private Context mCtx;
 
@@ -46,4 +50,45 @@ public class ListViewCriancaResp extends ArrayAdapter<CriancaConst> {
         return listViewItem;
     }
 
+    @Override
+    public int getCount() {
+        return criancaList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                final FilterResults oReturn = new FilterResults();
+                final ArrayList<CriancaConst> results = new ArrayList<CriancaConst>();
+                if (orig == null) {
+                    orig = criancaList;
+                }
+                if (constraint != null) {
+                    constraint = constraint.toString().toLowerCase();
+                    //verifica se temos algo na lista original
+                    if (orig != null && orig.size() > 0) {
+                        //para cada obj do UsuarioConst dentro lista
+                        for (final CriancaConst g : orig) {//Ocorerá para cada usuario que está na lista
+                            if ((g.getNomeCrianca().toLowerCase().contains(constraint.toString())) ||
+                                    (g.getCpfCrianca().toLowerCase().contains(constraint.toString())))
+                                     {
+                                results.add(g);
+                            }
+                        }
+                    }
+                    oReturn.values = results;
+                }
+                return oReturn;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                criancaList = (ArrayList<CriancaConst>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
 }
