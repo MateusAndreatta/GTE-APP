b107907e8afb408d40bd424137c7b6e9f7327f23
Andreatta <mateus-henrique2001@hotmail.com>
2018-05-17 01:41:54 -0300

    metodos de 'back'

--- app/src/main/java/xyz/sistemagte/gte/EditarMensalidade.java
+++ app/src/main/java/xyz/sistemagte/gte/EditarMensalidade.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -102,6 +103,26 @@ public class EditarMensalidade extends AppCompatActivity {
 
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, Mensalidades_adm.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    //O botao padrao do android
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this, Mensalidades_adm.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 
     public void Cadastrar(View view) {
