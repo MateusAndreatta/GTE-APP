16f85126bceeefb04b20be199b789ff9cca84ef5
Andreatta <mateus-henrique2001@hotmail.com>
2018-06-02 22:10:51 -0300

    xml enquete

--- app/src/main/java/xyz/sistemagte/gte/Enquete.java
+++ app/src/main/java/xyz/sistemagte/gte/Enquete.java
@@ -2,6 +2,7 @@ package xyz.sistemagte.gte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Enquete extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Enquete extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enquete);
+    }
+
+    public void votar(View view) {
+
+        //TODO: Request da reposta
+    }
+
+    public void voltar(View view) {
     }
 }
