4612c559355123abc7e50d1950ce33a2e9bef4b7
PedroErnestoCardozo <32229879+PedroErnestoCardozo@users.noreply.github.com>
2018-06-28 14:57:10 -0300

    voltar cad monitora e motorista no adm

--- app/src/main/java/xyz/sistemagte/gte/CadMonitoraAdm.java
+++ app/src/main/java/xyz/sistemagte/gte/CadMonitoraAdm.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
@@ -84,6 +85,8 @@ public class CadMonitoraAdm extends AppCompatActivity {
         hora_saida.addTextChangedListener(mascaraHoraSaida);
         tel_residencial.addTextChangedListener(mascaraTelResidencial);
         salario.addTextChangedListener(mascaraSalario);
+
+
 
         cep.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -215,6 +218,26 @@ public class CadMonitoraAdm extends AppCompatActivity {
         }else{
             return true;
         }
+    }
+    //este é para o da navbar (seta)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, Painel_adm.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    //O botao padrao do android
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this, Painel_adm.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 
     public void CadastrarMonitora(View view) {
--- app/src/main/java/xyz/sistemagte/gte/cad_motorista_adm.java
+++ app/src/main/java/xyz/sistemagte/gte/cad_motorista_adm.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
@@ -396,6 +397,26 @@ public class cad_motorista_adm extends AppCompatActivity {
                 return true;
             }
         }
+    }
+    //este é para o da navbar (seta)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:  //ID do seu botão (gerado automaticamente pelo android, usando como está, deve funcionar
+                startActivity(new Intent(this, Funcionario_adm.class));  //O efeito ao ser pressionado do botão (no caso abre a activity)
+                finishAffinity();  //Método para matar a activity e não deixa-lá indexada na pilhagem
+                break;
+            default:break;
+        }
+        return true;
+    }
+
+    //O botao padrao do android
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this, Funcionario_adm.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
     }
 
